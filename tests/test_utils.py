682fd79dbb52f8a4b53da0f2f7e1f2008ba20be4
Fernando Tricas García <ftricas@unizar.es>
2025-04-23 18:00:30 +0200

    ``` was incorrectly deleted

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -21,7 +21,8 @@ class TestUtils(unittest.TestCase):
     def test_extract_json(self):
         text = """Some text
 
-        json\n{\"key\": \"value\"}\n
+        ```json\n{\"key\": \"value\"}\n
+        ```
 
 more text"""
         expected_json = '{"key": "value"}'
