b9e19841ce426a7dde50da754d9dd4978dcebbc5
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2025-04-27 00:03:14 +0200

    changing import

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,18 +5,7 @@ import datetime
 import sys
 
 
-from manage_agenda.utils import (
-    extract_json,
-    process_event_data,
-    adjust_event_times,
-    create_event_dict,
-    select_message,
-    select_message_folder,
-    select_calendar,
-    safe_get,
-    LLMClient,
-    select_from_list,
-)
+from manage_agenda.utils import extract_json, process_event_data, adjust_event_times, create_event_dict, select_message, select_message_folder, select_calendar, safe_get, LLMClient, select_from_list
 
 
 class TestUtils(unittest.TestCase):
