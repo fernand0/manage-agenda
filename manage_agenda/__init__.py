660f4134265690445624f3afb0daf267186d08f9
Fernando Tricas García <fernand0@elmundoesimperfecto.com>
2025-04-26 20:08:58 +0200

    Modifying __init__.py to include utils

---
+++ manage_agenda/__init__.py
@@ -0,0 +1,13 @@
+from manage_agenda.utils import (
+    extract_json,
+    process_event_data,
+    adjust_event_times,
+    create_event_dict,
+    select_message,
+    select_message_folder,
+    select_calendar,
+    safe_get,
+    LLMClient,
+    select_from_list,
+)
+
