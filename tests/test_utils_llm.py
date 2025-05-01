77bf2dfc25f9e0e621f9e666fed12777191ff8ec
ftricas <ftricas@elmundoesimperfecto.com>
2025-05-01 17:59:05 +0000

    More tests

--- tests/test_utils_llm.py
+++ tests/test_utils_llm.py
@@ -2,9 +2,25 @@ import unittest
 from unittest.mock import patch, MagicMock
 
 from manage_agenda.utils_llm import LLMClient
+from manage_agenda.utils_llm import OllamaClient, GeminiClient, MistralClient
 
 
 class TestLLMUtils(unittest.TestCase):
     def test_init(self):
         llm_client = LLMClient()
         self.assertIsInstance(llm_client, LLMClient)
+
+    @patch("manage_agenda.utils_llm.genai.configure")
+    def test_get_name(self, mock_configure):
+        mock_configure.return_value = None
+        ollama_client = OllamaClient()
+        self.assertEqual(ollama_client.get_name(), "OllamaClient")
+
+        gemini_client = GeminiClient()
+        self.assertEqual(gemini_client.get_name(), "GeminiClient")
+
+        mistral_client = MistralClient()
+        self.assertEqual(mistral_client.get_name(), "MistralClient")
+
+
+
